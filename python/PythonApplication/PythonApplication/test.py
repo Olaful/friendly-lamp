78fb88e839ee4d6463af980c2bd7a9494eed0eb3
Olaful <40697682+Olaful@users.noreply.github.com>
2018-10-23 18:13:32 +0800

    test

--- python/PythonApplication/PythonApplication/test.py
+++ python/PythonApplication/PythonApplication/test.py
@@ -15,3 +15,20 @@ def square(x):
     25
     """
     return x*x
+
+def func2():
+    """
+    >>> from urllib.request import urlopen
+    >>> urlopen('http://127.0.0.1:8080/kamm').read().decode('utf-8')
+    >>> 1
+    """
+
+def func3(x,y):
+    if x == 7 and y == 9:
+        return 'unexpected numer'
+    else:
+        return x*y
+
+if __name__ == '__main__':
+    import doctest, test
+    doctest.testmod(test)
