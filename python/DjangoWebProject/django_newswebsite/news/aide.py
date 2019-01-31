d06aa3d1a6f8320ee83f4b5b1a9910ed0b74fb43
ta <1764740905@qq.com>
2019-01-31 10:47:01 +0800

    data-virsualization

--- python/DjangoWebProject/django_newswebsite/news/aide.py
+++ python/DjangoWebProject/django_newswebsite/news/aide.py
@@ -2,14 +2,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 import os
 import platform
+from matplotlib.font_manager import FontProperties
 
 def saveToBarh(datalist):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
     if platform.system() == 'Windows':
         static_dir = os.path.join(base_dir, 'static/images')
+        font = FontProperties(fname=os.path.join(base_dir + r'\static\fonts\wqydkzh.ttf'))
     elif platform.system() == 'Linux':
         static_dir = base_dir
+        fname = os.path.join(os.path.dirname(static_dir), r'/fonts/wqydkzh.ttf')
+        font = FontProperties(fname=fname)
 
     os.chdir(static_dir)
 
@@ -18,7 +22,8 @@ def saveToBarh(datalist):
     x.reverse()
     y.reverse()
 
-    matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+    #matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+    matplotlib.rcParams['axes.unicode_minus'] = False
 
     ax = plt.subplot2grid((1, 1), (0, 0))
     # 宽度0.5
@@ -28,19 +33,20 @@ def saveToBarh(datalist):
     ax.set_xticks(y)
     ax.set_yticks(ticks)
 
+    # 注解
     for i, _ in enumerate(x, start=1):
         xy = (y[i-1], i)
         plt.annotate(xy[0], xy=xy, xytext=(xy[0], i-1-0.1), xycoords='data')
 
     ax.set_xticks([])
     ax.set_facecolor('none')
-    ax.set_title('热门top 5(点赞数)')
+    ax.set_title('热门top 5(点赞数)', FontProperties=font)
 
     axes = plt.gca()
-    axes.spines['right'].set_color('none')
-    axes.spines['top'].set_color('none')
+    axes.spines['right'].set_visible(False)
+    axes.spines['top'].set_visible(False)
     #axes.spines['left'].set_color('none')
-    axes.spines['bottom'].set_color('none')
+    axes.spines['bottom'].set_visible(False)
 
     plt.setp(ax.get_yticklabels(), fontsize=15)
     plt.tight_layout()
